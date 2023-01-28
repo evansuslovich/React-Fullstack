70756e0af94018c4ca53319803631708004c1ff4
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-28 16:32:56 -0500

    getting post and get .. going to work on more CRUD features alter

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
 const db = require('./models')
  
+// Routers 
+const postRouter = require('./routes/Posts');
+app.use("/posts", postRouter)
+
+
 db.sequelize.sync().then( () => {
   app.listen(3001, () => {
     console.log("Server Running on port 3001")
