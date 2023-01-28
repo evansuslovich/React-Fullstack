405ef1cc3c0181798a956aab59cfc9662c3b161f
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-28 17:33:59 -0500

    cleaning up

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
+import "./App.css"
 import React from "react";
 import { useEffect } from "react";
 import axios from "axios"
+import { useState } from "react";
 
 function App() {
+  const [listOfPosts, setListOfPosts] = useState([])
 
   useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      console.log(response)
-    })
-  })
+    axios.get("http://localhost:3001/posts")
+      .then((response) => {
+        setListOfPosts(response.data)
+      });
+  }, [])
 
   return (
     <div>
-      <h1>hello</h1>
+      <ul>
+        {listOfPosts.map((list, key) => {
+          return (
+            <div className="container" key={key}>
+              <div className="post">
+                <div className="header">
+                  <h1>ID: {list.id}</h1>
+                  <h1>{list.title}</h1>
+                </div>
+
+                <h1>{list.username}</h1>
+                <h1>{list.postText}</h1>
+
+                <br />
+              </div>
+            </div>
+
+          )
+
+        })}
+      </ul>
     </div>
   );
 }
