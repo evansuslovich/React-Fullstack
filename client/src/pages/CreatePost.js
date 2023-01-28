6de80763398f6d869d635694074180bb37467546
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-28 18:39:04 -0500

    sending information to the backend

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios"
 
-function CreatePost() {
+
+export default function CreatePost() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.post("http://localhost:3001/posts", inputs)
+      .then((response) => {
+        console.log("success")
+      })
+
+  }
+
   return (
-    <div>CreatePost</div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="title"
+        value={inputs.title || ""}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="postText"
+        placeholder="postText"
+        value={inputs.postText || ""}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        value={inputs.username || ""}
+        onChange={handleChange}
+      />
+      <input type="submit" />
+    </form>
   )
 }
-
-export default CreatePost
