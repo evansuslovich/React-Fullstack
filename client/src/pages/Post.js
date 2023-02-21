3f385e30fb6de9bf989f023a4d8eaced2aa91fd6
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-02-21 14:22:13 -0500

    cleaning

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,14 +1,33 @@
-import React from "react";
-import  {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import axios from "axios"
 
+import "../../src/App.css"
+
+
 export default function Post() {
-  let { id } = useParams(); 
-  
+  let { id } = useParams();
+  const [post, setPost] = useState({})
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts/byId/" + id)
+      .then((response) => {
+        setPost(response.data)
+      })
+  }, [])
+
   return (
-    <div>
-    {id}
+    <div className="container">
+        <div className="header">
+          <h1>ID: {post.id}</h1>
+          <h1>Title: {post.title}</h1>
+        </div>
+
+        <h1>Username: {post.username}</h1>
+        <h1>Text: {post.postText}</h1>
+
+        <br />
     </div>
-    )
-  }
+  )
+}
