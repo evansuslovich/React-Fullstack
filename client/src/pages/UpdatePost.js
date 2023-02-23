b0e6d674fd49d0496c98a64c49891e92d3f295e5
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-02-23 12:58:55 -0500

    file for updating post .. could refactor to remove rewritten code

---
+++ client/src/pages/UpdatePost.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios"
+
+
+
+export default function UpdatePost() {
+  let { id } = useParams();
+  const [inputs, setInputs] = useState({});
+  const navigate = useNavigate()
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.put("http://localhost:3001/posts/" + id, inputs)
+      .then((response) => {
+        navigate("/")
+      })
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="title"
+        value={inputs.title || ""}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="postText"
+        placeholder="postText"
+        value={inputs.postText || ""}
+        onChange={handleChange}
+      />
+      <input type="submit" />
+    </form>
+  )
+}
